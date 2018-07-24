9d701c44ce5d82e9f6271502526c443fc3eef853
chitrasrinivasan12 <csriniv@ncsu.edu>
2018-07-24 01:31:43 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -2,8 +2,8 @@
 
 
 def crazy_strings("Hello", "Friends")
-  first_string = first_string.reverse.upcase
-  second_string = second_string.swapcase.gsub("s", "z")
+  Hello = first_string.reverse.upcase
+  Friends = second_string.swapcase.gsub("s", "z")
   return "first_string, second_string"
 end
 
