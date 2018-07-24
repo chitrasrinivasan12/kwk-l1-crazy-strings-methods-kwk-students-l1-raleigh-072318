7cbe112c3cb8d37b3907bda068c99ad8c09755ae
chitrasrinivasan12 <csriniv@ncsu.edu>
2018-07-24 01:36:21 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,7 @@
 # write your method here
 
 def crazy_strings("Hello", "Friends")
-  Hello = "Hello".reverse.upcase
-  Friends = "Friends".swapcase.gsub("s", "z")
-  return "#{Hello} #{Friends}"
+  S1 = "Hello".reverse.upcase
+  S2 = "Friends".swapcase.gsub("s", "z")
+  return S1, S2
 end
